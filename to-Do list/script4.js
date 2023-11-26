58744bc7df03a6877f183a8c84ac843bf90d6042
MeghashreeK <115255491+MeghashreeK@users.noreply.github.com>
2023-11-26 12:20:14 +0530

    Add files via upload

---
+++ to-Do list/script4.js
@@ -0,0 +1,81 @@
+let coding = JSON.parse(localStorage.getItem("coding")) || [];
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+const todoCount = document.getElementById("todoCount");
+const deleteButton = document.getElementById("deleteButton");
+const addButton = document.querySelector(".btn");
+
+document.addEventListener("DOMContentLoaded", function () {
+    addButton.addEventListener("click", addTask);
+    todoInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            addTask();
+        }
+    });
+    deleteButton.addEventListener("click", deleteAllTask);
+    displayTask();
+});
+function addTask() {
+    if(todoInput.value=="")
+    {
+        alert("Please enter a task");
+    }
+    else{
+    const newTask = todoInput.value.trim();
+    if (newTask !== "") {
+        coding.push(
+            {
+                text: newTask,
+                disabled: false,
+            }
+        );
+        saveToLocalStorage();
+        todoInput.value = "";
+        displayTask();
+    }
+}
+}
+function saveToLocalStorage() {
+    localStorage.setItem("coding", JSON.stringify(coding));
+}
+function displayTask() {
+    todoList.innerHTML = "";
+    coding.forEach((item, index) => {
+        const p = document.createElement("p");
+        p.innerHTML = `<div class="todo-container">
+        <input type="checkbox" class="todo-checkbox" id="input-${index}" ${item.disabled ? "checked":""}>
+        <p id="todo-${index}" id="para"class="${item.disabled ? "disabled":""}">${item.text}</p>
+        <div class="options">
+        <button class="btn" onClick="editTask(${index})">Edit</button>
+        <button class="btn" onClick="deleteTask(${index})">Delete</button>
+        </div>
+        </div>`;
+        p.querySelector(".todo-checkbox").addEventListener("change",()=>{
+            toggleTask(index);
+        });
+        todoList.appendChild(p);
+    });
+    todoCount.textContent=todo.length;
+}
+function editTask(index){
+    todoInput.value=coding[index].text;
+    coding.splice(index,1);
+    saveToLocalStorage();
+    displayTask();
+}
+function deleteTask(index){
+    coding.splice(index,1);
+    saveToLocalStorage();
+    displayTask();
+}
+function toggleTask(index){
+    coding[index].disabled=!coding[index].disabled;
+    saveToLocalStorage();
+    displayTask();
+}
+function deleteAllTask() {
+coding=[];
+saveToLocalStorage();
+displayTask();
+}
